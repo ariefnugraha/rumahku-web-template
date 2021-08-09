4220cb73d052ad94f21fcddb02597d6c42487420
Arief <muhammadariefnugraha77@gmail.com>
2021-08-09 22:30:03 +0700

    fix position of product item when product dot click

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    let productDot = $(".product-dot-container .product-dot");
+    let productItem = $(".product-item-container .product-item");
+    let windowWidth = $(window).width();
+    let i = 0;
 
     //NAVBAR TOGGLER
     $('.bar').click(function (e) {
@@ -6,28 +10,40 @@ $(document).ready(function () {
         $('.menu-container').toggle(500);
     })
 
-    //TUTUP SELURUH CARD PRODUCT APABILA GAMBAR DI KLIK
-    $(".homepage-image").click(function (e) { 
-        e.preventDefault();
-        $('.item-one').hide(500);
-        $('.item-two').hide(500);
-    });
+    for (i; i < productDot.length; i++) {
+        $(productDot).eq(i).click(function (e) {
+            e.preventDefault();
+            let productId = `#${$(this).attr("id")}.product-item`;
+            let offset = $(this).offset();
+            let offsetX = offset.left;
+            let offsetY = offset.top;
+            let xValue = offsetX + 210;
+            let yValue = offsetY + 10;
 
-    $(".dot-one").click(function (e) {
-        e.preventDefault();
-        $('.item-one').toggle(500);
-        $('.item-two').hide(500);
-    });
-
-    $(".dot-two").click(function (e) {
-        e.preventDefault();
-        $('.item-two').toggle(500);
-        $('.item-one').hide(500);
-    });
-
+            if($(window).width() >= 1440) {
+                xValue = offsetX + 245;
+                yValue = offsetY + 15;
+            }
+            
+            if(!$(productId).hasClass("show")) {
+                $(".product-item-container").addClass("show");
+                $(productId).addClass("show");
+                $(".product-item").not($(productId)).removeClass("show")
+                $(productId)
+                $(productId).css({
+                    'top': yValue,
+                    'left': xValue
+                })
+            } else {
+                console.log("NOT SHOW")
+                $(".product-item-container").removeClass("show")
+                $(productId).removeClass("show");
+            }
+        })
+    }
 
     //TUTUP SELURUH GAMBAR APABILA GAMBAR DALAM CAROUSEL DI KLIK DI KLIK
-    $(".carousel-image").click(function (e) { 
+    $(".carousel-image").click(function (e) {
         e.preventDefault();
         $('.item-carousel-one').hide(500)
         $('.item-carousel-two').hide(500);
@@ -35,21 +51,21 @@ $(document).ready(function () {
         $('#item-carousel-one').hide(500);
     });
 
-    $(".dot-carousel-one").click(function (e) { 
+    $(".dot-carousel-one").click(function (e) {
         e.preventDefault();
         $('.item-carousel-one').toggle(500);
         $('.item-carousel-two').hide(500);
         $('.item-carousel-three').hide(500);
     });
 
-    $(".dot-carousel-two").click(function (e) { 
+    $(".dot-carousel-two").click(function (e) {
         e.preventDefault();
         $('.item-carousel-two').toggle(500);
         $('.item-carousel-one').hide(500);
         $('.item-carousel-three').hide(500);
     });
 
-    $(".dot-carousel-three").click(function (e) { 
+    $(".dot-carousel-three").click(function (e) {
         e.preventDefault();
         $('.item-carousel-three').toggle(500);
         $('.item-carousel-one').hide(500);
@@ -60,44 +76,38 @@ $(document).ready(function () {
         e.preventDefault();
         $("#item-carousel-one").toggle(500);
     })
-
 })
 
-
 let i = 0;
-const teks = "temukan perabotan rumah yang modern dan berkualitas hanya di rumahku";
-const speed = 50;
-
 function typeWriter() {
-    if (i < teks.length) {
-        document.querySelector(".title").innerHTML += teks.charAt(i);
+    const speed = 50;
+    const text = "temukan perabotan rumah yang modern dan berkualitas hanya di rumahku";
+    if (i < text.length) {
+        document.querySelector(".title").innerHTML += text.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
 }
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
+// function toggleSlides(n) {
+//     showSlides(slideIndex += n);
+// }
 
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
 
-let slideIndex = 1;
-showSlides(slideIndex);
+// function showSlides(n) {
+//     let i;
+//     const slides = document.getElementsByClassName("carousel-content");
 
-function toggleSlides(n) {
-    showSlides(slideIndex += n);
-}
+//     if (n > slides.length) { slideIndex = 1 }
+//     if (n < 1) { slideIndex = slides.length }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    const slides = document.getElementsByClassName("carousel-content");
-
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-}
+//     slides[slideIndex - 1].style.display = "block";
+// }
